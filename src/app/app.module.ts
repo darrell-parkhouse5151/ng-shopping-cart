e4f93a77ae4d30932d055da94055731db85e7c74
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-14 02:01:28 -0500

    added the services to the providers array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+/* tslint:disable*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CachingServiceBase } from './services/caching.service';
+import { DeliveryOptionDataService } from './services/delivery-options.service';
+import { ProductDataService } from './services/product.service';
+import { LocalStorageService } from './services/storage.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        CachingServiceBase,
+        DeliveryOptionDataService,
+        ProductDataService,
+        LocalStorageService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
