b475ac560de9a0b17f2b18ac509fc6931a13ed92
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-03-14 12:22:18 -0500

    added the populate cart guard

---
+++ src/app/guard/populated-cart.route-guard.ts
@@ -0,0 +1,28 @@
+/* tslint:disable*/
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
+import {ShoppingCartService} from '../services/shopping-cart.service';
+
+@Injectable()
+export class PopulatedCardRouteGuard implements CanActivate {
+    public constructor(private router: Router,
+                       private shoppingCartService: ShoppingCartService) { }
+
+    public canActivate(): Observable<boolean> {
+        return new Observable<boolean>((observer: Observer<boolean>) => {
+            const cartSubscription = this.shoppingCartService.get()
+                .subscribe((cart) => {
+                    if (cart.items.length === 0) {
+                        observer.next(false);
+                    } else {
+                        observer.next(true);
+                    }
+                });
+
+            return () => cartSubscription.unsubscribe();
+        })
+    }
+}
